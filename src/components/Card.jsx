1ef2b09332b0bc42ce58ab752780aee5e203a5ff
Eduardo Cifuentes <edu.cifuentes@gmail.com>
2022-06-19 15:14:55 +0100

    feat: ch 25 game logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,17 @@
 const Card = ({ image, selected, onClick }) => {
 
+
   return (
     <div className="card">
       <div className={selected && 'selected'}>
-        <imag alt="" src={image} className='card-face'/>
+        <img alt="" src={image} className="card-face" />
+
         <img
           alt=""
           className="card-back"
           src={'/assets/fireship.png'}
           onClick={onClick}
         />
-
       </div>
     </div>
   );
