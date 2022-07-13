6143fac4c9c5434b1284ad3d9ecca435251aeff9
Eduardo Cifuentes <edu.cifuentes@gmail.com>
2022-07-13 09:46:37 +0100

    feat: ch25 add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Card from './components/Card';
+import Header from './components/Header';
 import shuffle from './utilities/shuffle';
 
 
@@ -22,6 +23,13 @@ function App() {
     setPickOne(null);
     setPickTwo(null);
     setDisabled(false);
+  };
+
+  // Start over
+  const handleNewGame = () => {
+    setWins(0);
+    handleTurn();
+    setCards(shuffle);
   };
 
   // Used for selection and match handling
@@ -77,7 +85,7 @@ function App() {
 
   return (
     <>
-
+      {/* <Header handleNewGame={handleNewGame} wins={wins} /> */}
       <div className="grid">
         {cards.map((card) => {
           const { image, id, matched } = card;
